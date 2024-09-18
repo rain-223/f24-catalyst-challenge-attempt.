002417aa7b898fd38e6fa042ab6180c25244846c
rain-223 <ef_rain125@protonmail.com>
2024-09-18 01:05:24 +0000

    Add files via upload

---
+++ Task1b.js
@@ -0,0 +1,45 @@
+// How do I run this script? 
+// node Task1b.js    
+
+todoList = []; 
+/**
+ * YOUR OBJECTIVE: 
+ * We do a lot in the addAndPrintTodos() funciton. It's also limited in
+ * in what it can do because it takes no input.
+ * Is there a way to seperate the concerns so that they could be tied to particular events
+ * while also allowing us to add any todos that we want?
+ *
+ * Replace the "__" in each of the "__todo" function definitions to create more logical 
+ * function names and define some logic to accomplish these tasks inspired by 
+ * what you did in Part A. Call both function in mainTask1b().
+ * 
+ * In addition to updating function names, only modify the file under 
+ * the "// YOUR CODE HERE" comments.
+ */
+
+function addTodo(todoString) {
+    todoList.push(todoString);
+}
+
+function printTodo() {
+    for (var i = 0; i < todoList.length(); i++) {
+        console.log(todoList[i]);
+    }
+}
+
+/**
+ * Main is considered the entry point to a procedural program. Within y/cs,
+ * it's rare for us to write procedures in JS, but for learning purposes
+ * we do it here
+ */
+function mainTask1b() {
+    addTodo();
+    printTodo();
+}
+
+// This bit of code ensures that a main method exists! If it doesn't, then it throws an error
+if (require.main === module) {
+    // unlike other languages like Java, there is no built in main method. We use a funciton called
+    // main by convention.
+    mainTask1b();
+}
